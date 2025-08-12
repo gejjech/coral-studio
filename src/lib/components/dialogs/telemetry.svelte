2ab3759cbb48b38dc0dbafe88f9ffd9026fa928e
Luka Forder <lukaforder@gmail.com>
2025-08-12 17:18:33 +0100

    fix: moved tele data into header of dialog and made messages scroll, header fixed at top

--- src/lib/components/dialogs/telemetry.svelte
+++ src/lib/components/dialogs/telemetry.svelte
@@ -16,6 +16,7 @@
 	import Separator from '../ui/separator/separator.svelte';
 	import Telemetry from '$lib/components/telemetry-message.svelte';
 	import { MessagesSquare } from '@lucide/svelte';
+	import { ScrollArea } from '../ui/scroll-area';
 
 	const filter = $state({
 		user: true,
@@ -66,124 +67,126 @@
 	{#if data}
 		<Dialog.Root bind:open>
 			<Dialog.Content
-				class="mx-auto flex h-[80%] max-h-[80%] w-full max-w-2xl min-w-[80%] flex-col overflow-scroll"
+				class="mx-auto flex h-[80%] max-h-[80%] w-full max-w-2xl min-w-[80%] flex-col overflow-hidden"
 			>
-				<Dialog.Header>
-					<Dialog.Title class="h-fit  font-[400]">Full Telemetry Data</Dialog.Title>
-					<span class="text-muted-foreground text-sm">{data.model_description} - {threadId}</span>
-				</Dialog.Header>
-				<Tabs.Root value="messages" class="flex flex-col gap-6">
-					<Tabs.List class="h-11 border  p-0.5">
-						<Tabs.Trigger value="messages"><Chat /> Message Insights</Tabs.Trigger>
-						<Tabs.Trigger value="details"><TextIndent /> Thread Details</Tabs.Trigger>
-						<Tabs.Trigger value="hyperparameters"><GlobeSimple /> Hyperparameters</Tabs.Trigger>
-					</Tabs.List>
-					<section class="flex items-center gap-4">
-						<Button variant="outline" class="rounded-full"
-							><Arrow class="rotate-90" /> Expand all</Button
-						>
-						<DropdownMenu.Root>
-							<DropdownMenu.Trigger>
-								{#snippet child({ props })}
-									<Button {...props} variant="outline">Filter</Button>
-								{/snippet}
-							</DropdownMenu.Trigger>
-							<DropdownMenu.Content class="w-56">
-								<DropdownMenu.Group>
-									<!-- forgive me for my sins (the way event counts are counted) -->
-									<DropdownMenu.Label>Show messages</DropdownMenu.Label>
-									<DropdownMenu.Separator />
-									<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.user}>
-										User <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'user').length}
-										</DropdownMenu.Shortcut>
-									</DropdownMenu.CheckboxItem>
-									<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.assistant}
-										>Agent <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'assistant').length}
-										</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-									>
-									<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.tool}
-										>Tool <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'tool').length}
-										</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-									>
-									<DropdownMenu.CheckboxItem
-										closeOnSelect={false}
-										bind:checked={filter.generic_assistant}
-										>Generic Assistant <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'generic_assistant')
-												.length}
-										</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-									>
+				<Tabs.Root value="messages" class="flex h-full flex-col gap-6">
+					<Dialog.Header>
+						<Dialog.Title class="h-fit  font-[400]">Full Telemetry Data</Dialog.Title>
+						<span class="text-muted-foreground text-sm">{data.model_description} - {threadId}</span>
+						<Tabs.List class="h-11 border  p-0.5">
+							<Tabs.Trigger value="messages"><Chat /> Message Insights</Tabs.Trigger>
+							<Tabs.Trigger value="details"><TextIndent /> Thread Details</Tabs.Trigger>
+							<Tabs.Trigger value="hyperparameters"><GlobeSimple /> Hyperparameters</Tabs.Trigger>
+						</Tabs.List>
+						<section class="flex items-center gap-4">
+							<Button variant="outline" class="rounded-full"
+								><Arrow class="rotate-90" /> Expand all</Button
+							>
+							<DropdownMenu.Root>
+								<DropdownMenu.Trigger>
+									{#snippet child({ props })}
+										<Button {...props} variant="outline">Filter</Button>
+									{/snippet}
+								</DropdownMenu.Trigger>
+								<DropdownMenu.Content class="w-56">
+									<DropdownMenu.Group>
+										<!-- forgive me for my sins (the way event counts are counted) -->
+										<DropdownMenu.Label>Show messages</DropdownMenu.Label>
+										<DropdownMenu.Separator />
+										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.user}>
+											User <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'user').length}
+											</DropdownMenu.Shortcut>
+										</DropdownMenu.CheckboxItem>
+										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.assistant}
+											>Agent <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'assistant').length}
+											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										>
+										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.tool}
+											>Tool <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'tool').length}
+											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										>
+										<DropdownMenu.CheckboxItem
+											closeOnSelect={false}
+											bind:checked={filter.generic_assistant}
+											>Generic Assistant <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'generic_assistant')
+													.length}
+											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										>
 
-									<DropdownMenu.CheckboxItem
-										closeOnSelect={false}
-										bind:checked={filter.generic_user}
-										>Generic User <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'generic_user').length}
-										</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										<DropdownMenu.CheckboxItem
+											closeOnSelect={false}
+											bind:checked={filter.generic_user}
+											>Generic User <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'generic_user').length}
+											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										>
+										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.developer}
+											>Developer <DropdownMenu.Shortcut
+												>{data.messages.data.filter((item) => item.role === 'developer').length}
+											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+										>
+									</DropdownMenu.Group>
+								</DropdownMenu.Content>
+							</DropdownMenu.Root>
+							<p class="text-muted-foreground text-sm">
+								Showing {data.messages.data.filter((item) => filter[item.role]).length} of {data
+									.messages.data.length} events
+							</p>
+						</section>
+					</Dialog.Header>
+					<ScrollArea class="min-h-0 flex-1 rounded-md pb-0">
+						<Tabs.Content value="messages" class="h-full  pb-4">
+							<ul class="flex flex-col [&_.separator:last-of-type]:hidden">
+								<!-- this can all be done better later, just proof of concept for now-->
+								{#each data.messages.data as message, i}
+									{#if filter.user && message.role === 'user'}
+										<Telemetry {message} />
+									{/if}
+									{#if filter.assistant && message.role === 'assistant'}
+										<Telemetry {message} />
+									{/if}
+									{#if filter.tool && message.role === 'tool'}
+										<Telemetry {message} />
+									{/if}
+									{#if filter.generic_assistant && message.role === 'generic_assistant'}
+										<Telemetry {message} />
+									{/if}
+									{#if filter.generic_user && message.role === 'generic_user'}
+										<Telemetry {message} />
+									{/if}
+									{#if filter.developer && message.role === 'developer'}
+										<Telemetry {message} />
+									{/if}
+								{/each}
+							</ul>
+						</Tabs.Content>
+						<Tabs.Content value="hyperparameters" class="p-2">empty</Tabs.Content>
+						<Tabs.Content value="details" class="flex w-full p-2"
+							><ol>
+								<li class="flex flex-col">
+									Additional parameters: <span class="bg-white/5"
+										>{JSON.stringify(data.additional_params)}</span
 									>
-									<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.developer}
-										>Developer <DropdownMenu.Shortcut
-											>{data.messages.data.filter((item) => item.role === 'developer').length}
-										</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-									>
-								</DropdownMenu.Group>
-							</DropdownMenu.Content>
-						</DropdownMenu.Root>
-						<p class="text-muted-foreground text-sm">
-							Showing {data.messages.data.filter((item) => filter[item.role]).length} of {data
-								.messages.data.length} events
-						</p>
-					</section>
-					<Tabs.Content value="messages" class="pb-4">
-						<ul class="flex flex-col [&_.separator:last-of-type]:hidden">
-							<!-- this can all be done better later, just proof of concept for now-->
-							{#each data.messages.data as message, i}
-								{#if filter.user && message.role === 'user'}
-									<Telemetry {message} />
-								{/if}
-								{#if filter.assistant && message.role === 'assistant'}
-									<Telemetry {message} />
-								{/if}
-								{#if filter.tool && message.role === 'tool'}
-									<Telemetry {message} />
-								{/if}
-								{#if filter.generic_assistant && message.role === 'generic_assistant'}
-									<Telemetry {message} />
-								{/if}
-								{#if filter.generic_user && message.role === 'generic_user'}
-									<Telemetry {message} />
-								{/if}
-								{#if filter.developer && message.role === 'developer'}
-									<Telemetry {message} />
-								{/if}
-							{/each}
-						</ul>
-					</Tabs.Content>
-					<Tabs.Content value="hyperparameters" class="p-2">empty</Tabs.Content>
-					<Tabs.Content value="details" class="flex w-full p-2"
-						><ol>
-							<li class="flex flex-col">
-								Additional parameters: <span class="bg-white/5"
-									>{JSON.stringify(data.additional_params)}</span
-								>
-							</li>
-							<li>Max tokens: {data.max_tokens}</li>
-							<li>Model description: {data.model_description}</li>
-							<li>Preamble: {data.preamble}</li>
-							<li class="flex flex-col">
-								Resources: <span class="bg-white/5">{JSON.stringify(data.resources)}</span>
-							</li>
-							<li>Temperature: {data.temperature}</li>
-						</ol>
-						<CodeBlock
-							text={JSON.stringify(data, null, 2)}
-							class=" overflow-scroll whitespace-pre-wrap"
-							language="json"
-						/>
-					</Tabs.Content>
+								</li>
+								<li>Max tokens: {data.max_tokens}</li>
+								<li>Model description: {data.model_description}</li>
+								<li>Preamble: {data.preamble}</li>
+								<li class="flex flex-col">
+									Resources: <span class="bg-white/5">{JSON.stringify(data.resources)}</span>
+								</li>
+								<li>Temperature: {data.temperature}</li>
+							</ol>
+							<CodeBlock
+								text={JSON.stringify(data, null, 2)}
+								class=" overflow-scroll whitespace-pre-wrap"
+								language="json"
+							/>
+						</Tabs.Content>
+					</ScrollArea>
 				</Tabs.Root>
 			</Dialog.Content>
 		</Dialog.Root>
