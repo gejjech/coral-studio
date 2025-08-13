80fff91fc4256d5db58b660310cd85baac24ed76
Luka Forder <lukaforder@gmail.com>
2025-08-13 16:00:57 +0100

    chore: fix colours of components to match theme and also remove expand option but with accordian placeholder for later

--- src/lib/components/dialogs/telemetry.svelte
+++ src/lib/components/dialogs/telemetry.svelte
@@ -7,6 +7,7 @@
 	import TextIndent from 'phosphor-icons-svelte/IconTextIndentRegular.svelte';
 	import GlobeSimple from 'phosphor-icons-svelte/IconGlobeSimpleRegular.svelte';
 	import CodeBlock from '$lib/components/code-block.svelte';
+	import Funnel from 'phosphor-icons-svelte/IconFunnelRegular.svelte';
 
 	import { type paths, type components } from '../../../generated/api';
 	import type { Session } from '$lib/session.svelte';
@@ -71,103 +72,108 @@
 			<Dialog.Content
 				class="mx-auto flex h-[80%] max-h-[80%] w-full max-w-2xl min-w-[80%] flex-col overflow-hidden"
 			>
-				<Tabs.Root value="messages" class="flex h-full flex-col gap-6">
-					<Dialog.Header>
+				<Tabs.Root value="messages" class="flex h-full flex-col gap-4">
+					<Dialog.Header class="flex flex-col gap-4">
 						<Dialog.Title class="h-fit  font-[400]">Full Telemetry Data</Dialog.Title>
 						<span class="text-muted-foreground text-sm">{data.model_description} - {threadId}</span>
-						<Tabs.List class="h-11 border  p-0.5">
+						<Tabs.List class="h-11 border p-0.5">
 							<Tabs.Trigger value="messages"><Chat /> Message Events</Tabs.Trigger>
 							<Tabs.Trigger value="details"><TextIndent /> Thread Details</Tabs.Trigger>
 							<Tabs.Trigger value="hyperparameters"><GlobeSimple /> Hyperparameters</Tabs.Trigger>
 						</Tabs.List>
-						<section class="flex items-center gap-4">
-							<Button variant="outline" class="rounded-full"
-								><Arrow class="rotate-90" /> Expand all</Button
-							>
-							<DropdownMenu.Root>
-								<DropdownMenu.Trigger>
-									{#snippet child({ props })}
-										<Button {...props} variant="outline">Filter</Button>
-									{/snippet}
-								</DropdownMenu.Trigger>
-								<DropdownMenu.Content class="w-56">
-									<DropdownMenu.Group>
-										<!-- forgive me for my sins (the way event counts are counted) -->
-										<DropdownMenu.Label>Show messages</DropdownMenu.Label>
-										<DropdownMenu.Separator />
-										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.user}>
-											User <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'user').length}
-											</DropdownMenu.Shortcut>
-										</DropdownMenu.CheckboxItem>
-										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.assistant}
-											>Agent <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'assistant').length}
-											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-										>
-										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.tool}
-											>Tool <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'tool').length}
-											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-										>
-										<DropdownMenu.CheckboxItem
-											closeOnSelect={false}
-											bind:checked={filter.generic_assistant}
-											>Generic Assistant <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'generic_assistant')
-													.length}
-											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-										>
-
-										<DropdownMenu.CheckboxItem
-											closeOnSelect={false}
-											bind:checked={filter.generic_user}
-											>Generic User <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'generic_user').length}
-											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-										>
-										<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.developer}
-											>Developer <DropdownMenu.Shortcut
-												>{data.messages.data.filter((item) => item.role === 'developer').length}
-											</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
-										>
-									</DropdownMenu.Group>
-								</DropdownMenu.Content>
-							</DropdownMenu.Root>
-							<p class="text-muted-foreground text-sm">
-								Showing {data.messages.data.filter((item) => filter[item.role]).length} of {data
-									.messages.data.length} events
-							</p>
-						</section>
 					</Dialog.Header>
 					<ScrollArea class="min-h-0 flex-1 rounded-md pb-0">
 						<Tabs.Content value="messages" class="h-full">
-							<ul class="flex flex-col px-4 [&_.separator:last-of-type]:hidden">
+							<section
+								class="bg-background absolute top-0 right-0 left-0 z-10 flex items-center gap-4 pb-4"
+							>
+								<!-- <Button variant="outline" class="rounded-full"
+								><Arrow class="rotate-90" /> Expand all</Button
+							> -->
+								<DropdownMenu.Root>
+									<DropdownMenu.Trigger>
+										{#snippet child({ props })}
+											<Button {...props}><Funnel /> Filter</Button>
+										{/snippet}
+									</DropdownMenu.Trigger>
+									<DropdownMenu.Content class="w-56">
+										<DropdownMenu.Group>
+											<!-- forgive me for my sins (the way event counts are counted) -->
+											<DropdownMenu.Label>Show messages</DropdownMenu.Label>
+											<DropdownMenu.Separator />
+											<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.user}>
+												User <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'user').length}
+												</DropdownMenu.Shortcut>
+											</DropdownMenu.CheckboxItem>
+											<DropdownMenu.CheckboxItem
+												closeOnSelect={false}
+												bind:checked={filter.assistant}
+												>Agent <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'assistant').length}
+												</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+											>
+											<DropdownMenu.CheckboxItem closeOnSelect={false} bind:checked={filter.tool}
+												>Tool <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'tool').length}
+												</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+											>
+											<DropdownMenu.CheckboxItem
+												closeOnSelect={false}
+												bind:checked={filter.generic_assistant}
+												>Generic Assistant <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'generic_assistant')
+														.length}
+												</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+											>
+
+											<DropdownMenu.CheckboxItem
+												closeOnSelect={false}
+												bind:checked={filter.generic_user}
+												>Generic User <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'generic_user')
+														.length}
+												</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+											>
+											<DropdownMenu.CheckboxItem
+												closeOnSelect={false}
+												bind:checked={filter.developer}
+												>Developer <DropdownMenu.Shortcut
+													>{data.messages.data.filter((item) => item.role === 'developer').length}
+												</DropdownMenu.Shortcut></DropdownMenu.CheckboxItem
+											>
+										</DropdownMenu.Group>
+									</DropdownMenu.Content>
+								</DropdownMenu.Root>
+								<p class="text-muted-foreground text-sm">
+									Showing {data.messages.data.filter((item) => filter[item.role]).length} of {data
+										.messages.data.length} events
+								</p>
+							</section>
+							<ul class="mt-16 flex flex-col px-4 [&_.separator:last-of-type]:hidden">
 								{#each data.messages.data as message, i}
 									<Telemetry {message} {filter} />
 								{/each}
 							</ul>
 						</Tabs.Content>
-						<Tabs.Content value="hyperparameters" class="p-2">empty</Tabs.Content>
+						<Tabs.Content value="hyperparameters" class="p-2">
+							<Card.Root>
+								<Card.Header>
+									<Card.Title>Additional parameters</Card.Title>
+								</Card.Header>
+								<Card.Content>
+									<CodeBlock
+										text={JSON.stringify(data.additional_params, null, 2)}
+										class="overflow-scroll whitespace-pre-wrap"
+										language="json"
+									/>
+								</Card.Content>
+							</Card.Root>
+						</Tabs.Content>
 						<Tabs.Content value="details" class="flex w-full flex-col gap-4 p-2">
 							<section class="grid grid-cols-2 gap-3">
 								<!-- TODO: pop these all into a tiny component maybe? -->
 								<ol class="flex flex-col gap-3">
-									<li>
-										<Card.Root>
-											<Card.Header>
-												<Card.Title>Additional parameters</Card.Title>
-											</Card.Header>
-											<Card.Content>
-												<CodeBlock
-													text={JSON.stringify(data.additional_params, null, 2)}
-													class="overflow-scroll whitespace-pre-wrap"
-													language="json"
-												/>
-											</Card.Content>
-										</Card.Root>
-									</li>
-
 									<li>
 										<Card.Root>
 											<Card.Header>
@@ -176,36 +182,6 @@
 											<Card.Content>
 												<CodeBlock
 													text={JSON.stringify(data.model_description, null, 2)}
-													class="overflow-scroll whitespace-pre-wrap"
-													language="json"
-												/>
-											</Card.Content>
-										</Card.Root>
-									</li>
-									<li>
-										<Card.Root>
-											<Card.Header>
-												<Card.Title>Resources</Card.Title>
-											</Card.Header>
-											<Card.Content>
-												<CodeBlock
-													text={JSON.stringify(data.resources, null, 2)}
-													class="overflow-scroll whitespace-pre-wrap"
-													language="json"
-												/>
-											</Card.Content>
-										</Card.Root>
-									</li>
-								</ol>
-								<ol class="flex flex-col gap-2">
-									<li>
-										<Card.Root>
-											<Card.Header>
-												<Card.Title>Preamble</Card.Title>
-											</Card.Header>
-											<Card.Content>
-												<CodeBlock
-													text={data.preamble}
 													class="overflow-scroll whitespace-pre-wrap"
 													language="json"
 												/>
@@ -235,6 +211,36 @@
 											<Card.Content>
 												<CodeBlock
 													text={JSON.stringify(data.temperature, null, 2)}
+													class="overflow-scroll whitespace-pre-wrap"
+													language="json"
+												/>
+											</Card.Content>
+										</Card.Root>
+									</li>
+								</ol>
+								<ol class="flex flex-col gap-2">
+									<li>
+										<Card.Root>
+											<Card.Header>
+												<Card.Title>Resources</Card.Title>
+											</Card.Header>
+											<Card.Content>
+												<CodeBlock
+													text={JSON.stringify(data.resources, null, 2)}
+													class="overflow-scroll whitespace-pre-wrap"
+													language="json"
+												/>
+											</Card.Content>
+										</Card.Root>
+									</li>
+									<li>
+										<Card.Root>
+											<Card.Header>
+												<Card.Title>Preamble</Card.Title>
+											</Card.Header>
+											<Card.Content>
+												<CodeBlock
+													text={data.preamble}
 													class="overflow-scroll whitespace-pre-wrap"
 													language="json"
 												/>
--- src/lib/components/telemetry-message.svelte
+++ src/lib/components/telemetry-message.svelte
@@ -4,8 +4,8 @@
 	import Wrench from 'phosphor-icons-svelte/IconWrenchRegular.svelte';
 	import CodeBlock from '$lib/components/code-block.svelte';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
-	import * as Tooltip from './ui/tooltip';
-	import { tools } from '$lib/mcptools';
+	import * as Accordion from '$lib/components/ui/accordion/index.js';
+	import { Button } from './ui/button';
 
 	let {
 		message,
@@ -55,51 +55,63 @@
 	const Icon = $derived(roleData.icon || null);
 </script>
 
+<!-- accordian code is here if we need it, but currently there isnt really anything to put in it -->
+
 {#if filter[message.role]}
+	<!-- <Accordion.Root class="relative" type="single">
+		<Accordion.Item value="item-1"> -->
 	<li
-		class={`bg-sidebar flex flex-row gap-4 rounded-lg border-2 p-4 py-6 text-sm ${roleData.borderClass}`}
+		class={`bg-card  flex flex-col gap-4 rounded-lg border-2 p-4 py-6 text-sm ${roleData.borderClass}`}
 	>
-		<div
-			class={`bg-input relative h-6 w-6 self-center rounded-lg p-5 *:absolute *:inset-0 *:m-auto *:h-6 *:w-6`}
-		>
-			{#if roleData.icon != null}
-				<Icon class={roleData.textClass} />
-			{:else}
-				<span class="text-muted-foreground">?</span>
-			{/if}
-		</div>
+		<div class="flex gap-4">
+			<div
+				class={`relative h-6 w-6 self-center rounded-lg bg-[#e5f0ff] p-5 *:absolute *:inset-0 *:m-auto *:h-6 *:w-6 dark:bg-[#1f2937]`}
+			>
+				{#if roleData.icon != null}
+					<Icon class={roleData.textClass} />
+				{:else}
+					<span class="text-muted-foreground">?</span>
+				{/if}
+			</div>
+			<section class="text-muted-foreground flex w-full flex-col gap-2">
+				<!-- <Accordion.Trigger class="absolute top-5 right-5 h-12 w-12 cursor-pointer"
+						></Accordion.Trigger> -->
 
-		<section class="text-muted-foreground flex w-full flex-col gap-2">
-			{#if message.content && message.content.length > 0}
-				<span class="text-foreground font-bold">{roleData.label}</span>
-
-				{#each message.content as part}
-					{#if message.role != 'user'}
-						<CodeBlock
-							text={part.text}
-							class=" max-w-full  whitespace-break-spaces "
-							language="json"
-						/>
-					{:else}
-						<p class="whitespace-pre-wrap">{part.text}</p>
-					{/if}
-				{/each}
-			{:else if message.tool_calls}
-				<span class="text-foreground font-bold">Agent invoked tools</span>
-				<ul>
-					{#each message.tool_calls as toolCall}
-						<li>
-							<span class="font-bold">{toolCall.function.name}</span>
-
-							<CodeBlock text={toolCall.function.arguments} class="max-w-max" language="json" />
-						</li>
+				{#if message.content && message.content.length > 0}
+					<span class="text-foreground font-bold">{roleData.label}</span>
+					{#each message.content as part}
+						{#if message.role != 'user'}
+							<CodeBlock
+								text={part.text}
+								class=" max-w-full  whitespace-break-spaces "
+								language="json"
+							/>
+						{:else}
+							<p class="whitespace-pre-wrap">{part.text}</p>
+						{/if}
 					{/each}
-				</ul>
-			{:else}
-				<p class="text-gray-400 italic">No content</p>
-			{/if}
-		</section>
+				{:else if message.tool_calls}
+					<span class="text-foreground font-bold">Agent invoked tools</span>
+					<ul>
+						{#each message.tool_calls as toolCall}
+							<li>
+								<span class="font-bold">{toolCall.function.name}</span>
+								<CodeBlock text={toolCall.function.arguments} class="max-w-max" language="json" />
+							</li>
+						{/each}
+					</ul>
+				{:else}
+					<p class="text-gray-400 italic">No content</p>
+				{/if}
+			</section>
+		</div>
+		<!-- <Accordion.Content class="flex flex-col gap-2">
+					<Separator />
+					
+				</Accordion.Content> -->
 	</li>
+	<!-- </Accordion.Item>
+	</Accordion.Root> -->
 
 	<Separator orientation="vertical" class="separator ml-6 h-6 max-h-6 min-h-6" />
 {/if}
