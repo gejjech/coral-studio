4550655cbaeffb50ec2ac0945ece96eb41d19b61
Luka Forder <lukaforder@gmail.com>
2025-09-23 16:32:23 +0100

    wip

--- src/routes/tools/user-input/+page.svelte
+++ src/routes/tools/user-input/+page.svelte
@@ -3,15 +3,18 @@
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb';
 	import * as Sidebar from '$lib/components/ui/sidebar';
 
+	import IconRobot from 'phosphor-icons-svelte/IconRobotRegular.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import { Separator } from '$lib/components/ui/separator';
 
 	import { socketCtx } from '$lib/socket.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	let ctx = socketCtx.get();
 	let userQuestions: Record<string, string> = $state({});
 
 	let requests = $derived(Object.values(ctx.userInput.requests ?? {}));
+
 	$effect(() => {
 		for (const request of requests) {
 			if (request.userQuestion !== undefined) {
@@ -19,6 +22,16 @@
 			}
 		}
 	});
+
+	function stringToColor(str: string): string {
+		let hash = 0;
+		for (let i = 0; i < str.length; i++) {
+			hash = str.charCodeAt(i) + ((hash << 5) - hash);
+		}
+		let color = (hash >>> 0).toString(16); // unsigned
+		// pad/truncate to 6 chars
+		return '#' + color.padStart(6, '0').slice(0, 6);
+	}
 </script>
 
 <header class="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -36,10 +49,7 @@
 		</Breadcrumb.List>
 	</Breadcrumb.Root>
 </header>
-<main class="grid grid-cols-1 gap-3 p-4 md:grid-cols-2 lg:grid-cols-3">
-	{#if requests.length == 0}
-		<p class="text-muted-foreground col-span-full text-center text-sm">No requests yet.</p>
-	{/if}
+<section class="grid h-full grid-cols-2 gap-3 p-4 md:grid-cols-3 lg:grid-cols-4">
 	{#each requests as request}
 		<Card.Root>
 			<Card.Header>
@@ -62,4 +72,73 @@
 			</Card.Content>
 		</Card.Root>
 	{/each}
-</main>
+	<section class="flex h-full w-full flex-col gap-4">
+		<nav class="flex w-full flex-col gap-2 p-1">
+			<h1 class="text-lg font-medium">{requests.length} Input requests</h1>
+			<section class="flex w-full gap-2">
+				<Input placeholder="Search" class="grow " />
+				<Button class="">Search</Button>
+			</section>
+		</nav>
+		<Card.Root>
+			<Card.Content class="flex w-full flex-col gap-2">
+				{#if requests.length === 0}
+					<p class="text-muted-foreground col-span-full text-center text-sm">
+						No agents have requested user input.
+					</p>
+				{/if}
+				{#each requests as request}
+					<div class="flex w-full items-center gap-2">
+						<div
+							class="h-12 min-w-12 justify-center rounded-md bg-[{stringToColor(request.agentId)}]"
+						>
+							<IconRobot class="m-auto size-6 h-full" />
+						</div>
+						<section class="flex w-full flex-col gap-1">
+							<span class="flex w-full">
+								<span class="grow">{request.agentId}</span>
+								<span class="w-fit">1 hour ago</span>
+							</span>
+							<p class="text-muted-foreground truncate">{request.agentRequest}</p>
+						</section>
+					</div>
+				{/each}
+			</Card.Content>
+		</Card.Root>
+	</section>
+	<section class="col-span-3 h-full w-full">
+		<Card.Root class=" h-full ">
+			<Card.Header>
+				<Card.Title class="flex w-full items-center gap-2">
+					<div class="h-12 min-w-12 justify-center rounded-md bg-green-400">
+						<IconRobot class="m-auto size-6 h-full" />
+					</div>
+					<span class="grow">Agent title</span>
+				</Card.Title>
+			</Card.Header>
+			<Separator />
+			<Card.Content class="grow">
+				<ol class="flex flex-col gap-4">
+					{#each requests as request}
+						<li class="flex gap-4">
+							<div
+								class="h-12 min-w-12 justify-center rounded-md bg-[{stringToColor(
+									request.agentId
+								)}]"
+							>
+								<IconRobot class="m-auto size-6 h-full" />
+							</div>
+							<div class="bg-input min-h-12 max-w-full rounded-md p-2">
+								<p>{request.agentRequest}</p>
+							</div>
+						</li>
+					{/each}
+				</ol>
+			</Card.Content>
+			<Card.Footer class="flex gap-2 p-4">
+				<Input placeholder="Type your message..." class="w-full grow" />
+				<Button>Send</Button>
+			</Card.Footer>
+		</Card.Root>
+	</section>
+</section>
